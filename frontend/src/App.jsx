bbded12e43fbe8e06639607a32009125e5cc589d
Kushals-Air <kushalchoudhary3121@gmail.com>
2025-07-20 23:02:02 -0500

    Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>DataTrail Landing</h1>
+    </div>
+  );
+}
+
+export default App;
